74a8e552c6c1cbe3d08dbf8c8678f424cfb3334e
Uday Chugh <61309187+udaychugh@users.noreply.github.com>
2021-06-15 12:07:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("getData").onclick = function () {
+    // taking values from html
+
+    var date = document.getElementById("date").value;
+    var month = document.getElementById("month").value;
+    var year = document.getElementById("year").value;
+    var time = document.getElementById("time").value;
+    
+    var currentDate = new Date();
+    
+    var newDay = currentDate.getDate();
+    var newMonth = currentDate.getMonth();
+    var newYear = currentDate.getFullYear();
+    var newHour = currentDate.getHours();
+    var newMin = currentDate.getMinutes();
+    
+    var finalDate = newDay - date;
+    var finalMonth = newMonth - month;
+    var finalYear = newYear - year;
+    var finalTime = (newHour + newMin) - time;
+    
+    document.getElementById("showAge").innerHTML = finalDate + finalMonth + finalYear + finalTime;
+}
